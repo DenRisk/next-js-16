e7603ed9ba503c5c288683f225d6c4c4f1712e5d
Denis Niklas Risken <Denis.R.95@hotmail.de>
2025-12-23 13:20:20 +0100

    Fix Suspense Bug and page reload

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ const EventsPage = async () => {
 
             <ExploreBtn/>
 
-            <div className="mt-20 space-y-7">
+            <div id='events' className="mt-20 space-y-7">
                 <h3>Featured Events</h3>
 
                 <ul className="events list-none">
